a29594416051964380060f863aa7f0b5e6f463d2
spa1teN <82121424+spa1teN@users.noreply.github.com>
2023-02-24 12:43:30 +0100

    Add files via upload

---
+++ fick.py
@@ -0,0 +1,41 @@
+import json
+import tweepy
+
+#Twitter Authentication
+api_key = ""
+api_secret = ""
+bearer_token = ""
+access_token = ""
+access_token_secret = ""
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#Word-Picking
+file = open("wortliste.txt", encoding="ISO-8859-1")
+
+with open ("/home/pi/fick/stand.txt", "r") as stand:
+    n = int(stand.read())
+    print("Stand: " + str(n))
+    stand.close()
+    pass
+
+lines=file.readlines()
+
+line = lines[n]
+
+output = "fick " + line
+
+print(output)
+
+'Closing
+client.create_tweet(text=output)
+
+with open ("stand.txt", 'w') as stand:
+    nn = n + 1
+    stand.write(str(nn))
+    stand.close()
+    pass
+
+print("\ntweeted")
+
